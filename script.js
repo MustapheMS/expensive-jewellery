e2b365de182a072045f4e7e572f234c90e2f823d
MustapheMS <moztaphem9@gmail.com>
2023-06-13 20:42:03 +0300

    Expensive Jewellery Done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+
+// done 💯💯
+function mostExpensive(){
+    mostExpensive = {
+        Diamond_Earrings: 980,
+        Gold_Watch: 250,
+        Pearl_Necklace: 4650,
+      } 
+    }
+    console.log("THE MOS EXPENSIVE ONE IS THE PEARL NECKLACE")
